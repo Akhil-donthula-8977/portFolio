aa3ed5ab4352deb6a46e692be8159e7405157ca5
Akhil <akhil.donthula8977@gmail.com>
2023-08-06 16:27:47 +0530

    "routing"

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require("express")
-const mongoose=require("./mongoose/mongoose")
+const mongoose = require("./mongoose/mongoose")
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const app = express();
-const Form=require("./models/form")
+const Form = require("./models/form")
 
 
 //body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
@@ -15,24 +15,29 @@ app.use(express.static("frontend"))
 app.get("/", (req, res) => {
     res.render("index")
 })
-app.get("/contact", (req, res) => {
+app.get("/contact",async (req, res) => {
+  try{
     res.render("contactpage")
+  }
+  catch(e){
+      res.status(400).send("on the way :)");
+  }
 })
-app.post("/contact",async  (req, res) => {
+app.post("/contact", async (req, res) => {
 
-    try{
-     
-      
-      const form=new Form(req.body)
-      await form.save();
-      res.status(200).redirect("/")
-    }catch(e){
-     
+    try {
+
+
+        const form = new Form(req.body)
+        await form.save();
+        res.status(200).redirect("/")
+    } catch (e) {
+
         res.status(400).send("400 bad requestt :(")
     }
-  
+
 })
 
 app.listen(process.env.PORT, (err) => {
-    
+
 })
