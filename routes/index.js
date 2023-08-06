d74f700ea4fca8dd6c610adc446e82cad0fc981d
Akhil <akhil.donthula8977@gmail.com>
2023-08-06 15:43:43 +0530

    ":)"

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require("express")
-const mongoose=require("./mongoose/mongoose");
+const mongoose=require("../mongoose/mongoose")
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const app = express();
-const Form=require("./models/form")
+const Form=require("../models/form")
 
 
 //body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))//must use
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("frontend"))
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index")
 })
 app.get("/contact", (req, res) => {
@@ -25,7 +25,7 @@ app.post("/contact",async  (req, res) => {
       
       const form=new Form(req.body)
       await form.save();
-      res.status(200).redirect("/home")
+      res.status(200).redirect("/")
     }catch(e){
      
         res.status(400).send("400 bad request :(")
